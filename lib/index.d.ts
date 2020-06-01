cde61176e3fc0201e21342d12ee17b957c3fec73
Jerry Kim <jerryrox@live.co.kr>
2020-06-01 15:48:36 +1200

    Remove libs folder from repo

--- lib/index.d.ts
+++
@@ -1,6 +0,0 @@
-import Bindable from './Bindable';
-import { useBindable } from './Bindable';
-import BaseBloc from './bloc/BaseBloc';
-import BlocContextValue from './bloc/BlocContextValue';
-import BlocEvent from './bloc/BlocEvent';
-export { Bindable, BaseBloc, BlocContextValue, BlocEvent, useBindable };
