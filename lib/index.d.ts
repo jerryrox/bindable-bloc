674d92be24d34aa644f6f7effa563bcce2ae85e0
Jerry Kim <jerryrox@live.co.kr>
2021-06-20 14:05:48 +1200

    Increased version

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -2,6 +2,6 @@ import Bindable from './Bindable';
 import BaseBloc from './bloc/BaseBloc';
 import BlocContextValue from './bloc/BlocContextValue';
 import BlocEvent from './bloc/BlocEvent';
-import useBindable from './utils/useBindable';
+import useBindable, { useBindableUnsafe } from './utils/useBindable';
 import { makePersistent } from './utils/PersistentState';
-export { Bindable, BaseBloc, BlocContextValue, BlocEvent, useBindable, makePersistent };
+export { Bindable, BaseBloc, BlocContextValue, BlocEvent, useBindable, useBindableUnsafe, makePersistent };
